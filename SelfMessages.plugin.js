b380b2602772e72edc7536f302bf20ae5a53911a
N14 <Riddim_GLiTCH@courvix.com>
2023-07-03 23:04:32 +0200

    Update SelfMessages.plugin.js

--- SelfMessages.plugin.js
+++ SelfMessages.plugin.js
@@ -2,92 +2,142 @@
  * @name SelfMessages
  * @author Riddim_GLiTCH
  * @authorId 801089753038061669
- * @version 1.0
+ * @version 1.1
  * @description BD theme helper plugin, Marks messages sent by the currently logged in user with an element for use with themes.
  * @invite aYxpgkvdvR
  * @source https://github.com/Riddim-GLiTCH/BD-SelfMessages
  */
-
-module.exports = class MyPlugin {
-  constructor(meta) {
-    // Do stuff in here before starting
-  }
-
-  start() {
-    // Do stuff when enabled
-    if (this._observer) this._observer.disconnect();
-
-const currentUserId = BdApi.Webpack.getModule(m => m?._dispatchToken && m.getCurrentUser).getCurrentUser().id;
-
-const getReactProps = (element, filter) => {
-    const instance = element[Object.keys(element).find(e => e.startsWith("__reactProps"))];
-
-    if (!instance) return;
-
-    let stack = [instance], i = 100;
-    
-    while (stack.length && i--) {
+class SelfMessages {
+    constructor() {
+        console.log("SelfMessages enabled. Your userID is:", BdApi.Webpack.getModule(m => m?._dispatchToken && m.getCurrentUser).getCurrentUser().id);
+    }
+  
+    start() {
+      // Do tasks when enabled
+      this.observeMessages();
+      this.markSelfMessages();
+    }
+  
+    stop() {
+      // Cleanup when disabled
+      this.disconnectObserver();
+      console.log ("SelfMessages stopped")
+    }
+  
+    observeMessages() {
+      this.observer = new MutationObserver((mutations) => {
+        for (const mutation of mutations) {
+          if (mutation.type === "attributes" && mutation.attributeName === "class") {
+            const className = Object.keys(this.injections).find((key) => mutation.target.classList?.contains(key));
+            if (className) {
+              this.injections[className]([mutation.target]);
+            }
+          }
+  
+          if (mutation.addedNodes.length) {
+            this.handleAddedNodes(mutation.addedNodes);
+          }
+        }
+      });
+  
+      this.observer.observe(document.body, { childList: true, subtree: true, attributes: true });
+    }
+  
+    handleAddedNodes(addedNodes) {
+      for (const node of addedNodes) {
+        if (node.nodeType === Node.TEXT_NODE) {
+          continue;
+        }
+  
+        for (const className in this.injections) {
+          const elements = node.querySelectorAll(`.${className}`);
+          if (elements.length) {
+            this.injections[className](elements);
+          }
+        }
+      }
+    }
+  
+    markSelfMessages() {
+      const currentUserId = this.getCurrentUser()?.id;
+      if (!currentUserId) {
+        console.error("Failed to get current user ID.");
+        return;
+      }
+  
+      const messageElements = Array.from(document.querySelectorAll(".message-2CShn3"));
+  
+      for (const el of messageElements) {
+        if (!el.classList.contains("selfmessage")) {
+          const props = this.getReactProps(el, (e) => e.message);
+          if (props && props.message?.author?.id === currentUserId) {
+            el.classList.add("selfmessage");
+          }
+        }
+      }
+    }
+  
+    getReactProps(element, filter) {
+      const reactPropsKey = Object.keys(element).find((key) => key.startsWith("__reactProps"));
+      const instance = element[reactPropsKey];
+  
+      if (!instance) {
+        return null;
+      }
+  
+      const stack = [instance];
+      let i = 100;
+  
+      while (stack.length && i--) {
         const curr = stack.shift();
-
-        if (!curr) continue;
-        if (filter(curr)) return curr;
-        
-        if (Array.isArray(curr)) stack.push(...curr);
-        else if (curr.children) stack.push(curr.children);
-        else if (curr.props) stack.push(curr.props);
-    }
-}
-
-{
-    const Injections = {
-        "message-2CShn3": elements => {
-            for (const el of elements) {
-                if (el.classList?.contains("is-current-user")) continue;
-                const props = getReactProps(el, e => e.message);
-                if (!props || props.message?.author?.id !== currentUserId) continue;
-                el.classList.add("is-current-user");
-            }
+  
+        if (!curr) {
+          continue;
         }
+  
+        if (filter(curr)) {
+          return curr;
+        }
+  
+        if (Array.isArray(curr)) {
+          stack.push(...curr);
+        } else if (curr.children) {
+          stack.push(curr.children);
+        } else if (curr.props) {
+          stack.push(curr.props);
+        }
+      }
+  
+      return null;
+    }
+  
+    getCurrentUser() {
+      const module = BdApi.Webpack.getModule((m) => m?._dispatchToken && m.getCurrentUser);
+      return module?.getCurrentUser?.();
+    }
+  
+    disconnectObserver() {
+      if (this.observer) {
+        this.observer.disconnect();
+        this.observer = null;
+      }
+    }
+  
+    injections = {
+      "message-2CShn3": (elements) => {
+        for (const el of elements) {
+          if (el.classList?.contains("selfmessage")) {
+            continue;
+          }
+  
+          const props = this.getReactProps(el, (e) => e.message);
+          if (props && props.message?.author?.id === this.getCurrentUser()?.id) {
+            el.classList.add("selfmessage");
+          }
+        }
+      },
     };
-
-    const obs = this._observer = new MutationObserver(changes => {
-        for (const mutation of changes) {
-            if (mutation.type === "attributes" && mutation.attributeName === "class") {
-               const name = Object.keys(Injections).find(k => mutation.target.classList?.contains(k));
-                if (!name) continue;
-
-                Injections[name]([mutation.target]);
-            }
-
-            if (!mutation.addedNodes.length) continue;
-            
-            for (const added of mutation.addedNodes) {
-                if (added.nodeType === Node.TEXT_NODE) continue;
-
-                for (const className in Injections) {
-                    const elements = Array.from(added.getElementsByClassName(className));
-
-                    if (elements.length) Injections[className](elements);
-                }
-            }
-        }
-    });
-
-    obs.observe(document.body, {childList: true, subtree: true, attributes: true});
-
-    
-    for (const className in Injections) {
-        const elements = Array.from(document.getElementsByClassName(className));
-    
-        if (!elements.length) continue;
-    
-        Injections[className](elements)
-    }
-}
   }
-
-  stop() {
-    // Cleanup when disabled
-    this._observer.disconnect()
-  }
-};
+  
+  module.exports = SelfMessages;
+  
